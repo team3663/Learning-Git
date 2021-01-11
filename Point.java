6291896089a611d81752acb4d5411c8ea06a4041
嘉佳 <frankwyang@users.noreply.github.com>
2021-01-11 09:28:11 -0800

    reset repository

---
+++ Point.java
@@ -0,0 +1,16 @@
+public class Point {
+    private int x;
+    private int y;
+
+    // need constructor(s)
+
+    // need setter methods
+
+    // need getter methods
+    
+    // need translation methods
+
+    // need distanceTo() method
+
+    // need toString() method
+}
