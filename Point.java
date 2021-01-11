7e2199147568b55425f9f040e2a034c95a8cb7a8
嘉佳 <frankwyang@users.noreply.github.com>
2021-01-11 10:26:43 -0800

    add class variables and methods and quadrant()

--- Point.java
+++ Point.java
@@ -1,7 +1,7 @@
 public class Point {
     // need private variables x and y
 
-    // need constructor(s)
+    // need constructor(s) 0 argument, 2 arguments
 
     // need setter methods
 
@@ -12,4 +12,10 @@ public class Point {
     // need distanceTo() method
 
     // need toString() method
+
+    // need a class variable to keep track total points (put it on top)
+
+    // need to class method to return total points (put it just below constructors)
+
+    // need a quadrant() method that return which quadrant the point is located 1, 2, 3, or 4
 }
