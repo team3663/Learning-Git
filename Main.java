8b31af1dccdb458bd124a3d95a734916ac249476
嘉佳 <frankwyang@users.noreply.github.com>
2021-01-11 11:15:21 -0800

    completed Main.jav and Point.java

--- Main.java
+++ Main.java
@@ -3,5 +3,19 @@ public class Main {
         System.out.println("Hello World!");
 
         // need testing code
+        Point p1 = new Point(3, 4);     // constructor & toString()
+        System.out.println(p1);         
+
+        p1.setX(11);                    // setter methods
+        p1.setY(10);
+        System.out.println(p1.getX());  // getter methods
+        System.out.println(p1.getY());
+
+        p1.dx(-6);                      // translation methods
+        p1.dy(2);
+        System.out.println(p1);
+
+        Point origin = new Point(0, 0); // distanceTo() method
+        System.out.println("Distance from " + origin + " to " + p1 + " is " + p1.distanceTo(origin));
     }
 }
--- Point.java
+++ Point.java
@@ -3,14 +3,45 @@ public class Point {
     private int y;
 
     // need constructor(s)
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     // need setter methods
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     // need getter methods
-    
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     // need translation methods
+    public void dx(int x) {
+        this.x += x;
+    }
+
+    public void dy(int y) {
+        this.y += y;
+    }
 
     // need distanceTo() method
+    public double distanceTo(Point other) {
+        return Math.sqrt(Math.pow(this.getX() - other.getX(), 2) + Math.pow(this.getY() - other.getY(), 2));
+    }
 
     // need toString() method
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
