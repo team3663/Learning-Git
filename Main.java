148ecdf484457d5c2aedde4797bfbe07c400df59
嘉佳 <frankwyang@users.noreply.github.com>
2021-01-12 08:55:13 -0800

    ok learned how to merge. now reset Main.java in master branch

--- Main.java
+++ Main.java
@@ -2,10 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        // dev branch is ahead at this line
-
         // need testing code
 
-        // move master branch ahead 
     }
 }
