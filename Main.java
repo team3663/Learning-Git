dbccabc91d5867bbb53bbeb8ad2a3e6ad570a51b
嘉佳 <frankwyang@users.noreply.github.com>
2021-01-11 16:38:19 -0800

    add Numbers in Java

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Numbers in Java ===== https://www.w3schools.com/java/java_data_types.asp");
+        System.out.println("short (2-byte): " + Short.MAX_VALUE + " ~ " + Short.MIN_VALUE);
+        System.out.println("int (4-byte): " + Integer.MAX_VALUE + " ~ " + Integer.MIN_VALUE);
+        System.out.println("long (8-byte): " + Long.MAX_VALUE + " ~ " + Long.MIN_VALUE);
+        System.out.println("float (4-byte): " + Float.MAX_VALUE + " ~ " + Float.MIN_VALUE);
+        System.out.println("double (8-byte): " + Double.MAX_VALUE + " ~ " + Double.MIN_VALUE);
+        
 
         // need testing code
         Point p1 = new Point(3, 4);     // constructor & toString()
